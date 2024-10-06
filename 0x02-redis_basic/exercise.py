e417ebccfc2eb7ad0e2cda1ca221b9e1b402915b
Elizabeth <elizase405@gmail.com>
2024-10-06 18:34:51 +0000

    feat: task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """A module with 1 Class: Cache"""
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable
 
 
 class Cache:
@@ -23,3 +23,29 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get_str(self, data: bytes) -> str:
+        ''' automatically parametrize Cache.get
+            with str conversion function.
+        '''
+        return str(data)
+
+    def get_int(self, data: bytes) -> int:
+        ''' automatically parametrize Cache.get
+            with the int conversion function.
+        '''
+        return int(data)
+
+    def get(self,
+            key: str,
+            fn: Optional[Callable]
+            ) -> Union[str, bytes, int, float]:
+        '''Convert data to desired format'''
+        value = self._redis.get(key)
+        if isinstance(fn, int):
+            return self.get_int(value)
+        if isinstance(fn, str):
+            return self.get_str(value)
+        if fn is None:
+            return value
+        return fn(value)
